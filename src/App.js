9ff7c1500fb9b1ce81b6be55ad321703132d2e3d
Mohit kumar <46507817+Mohitkumar33@users.noreply.github.com>
2022-04-14 09:32:26 +0530

    chore: fixed errors

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { LandingPage } from "./components/landingPage";
 import { DetailedLanding } from "./components/detailedLanding";
 import { useState } from "react";
 const App = () => {
-  const [userEntered, setUserEntered] = useState();
+  const [userEntered, setUserEntered] = useState(localStorage.user);
   return (
     <div className="App">
-      {localStorage.user ? (
-        <DetailedLanding userEntered={localStorage.user} />
+      {userEntered ? (
+        <DetailedLanding userEntered={userEntered} />
       ) : (
         <LandingPage setUserEntered={setUserEntered} />
       )}
