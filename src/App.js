c1b37f22f73a8891101497bce401589c294c01bd
Mohit kumar <46507817+Mohitkumar33@users.noreply.github.com>
2022-04-16 11:17:50 +0530

    chore : background image made some dark

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { LandingPage } from "./components/landingPage";
 import { DetailedLanding } from "./components/detailedLanding";
 import { useState } from "react";
+import "./app.css";
 
 const App = () => {
   const [userEntered, setUserEntered] = useState(localStorage.user);
   return (
-    <div className="App">
+    <div className="App darken">
       {userEntered ? (
         <DetailedLanding userEntered={userEntered} />
       ) : (
