eec60e9149df4d67e58500025fe83f9dd86e644e
Mohit kumar <46507817+Mohitkumar33@users.noreply.github.com>
2022-04-17 17:34:07 +0530

    chore: added setting functionality

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import { useState } from "react";
 import "./app.css";
 
 const App = () => {
-  const [userEntered, setUserEntered] = useState(localStorage.user);
+  const [userEntered, setUserEntered] = useState(
+    localStorage.user ? localStorage.user : false
+  );
   return (
     <div className="App darken">
       {userEntered ? (
-        <DetailedLanding userEntered={userEntered} />
+        <DetailedLanding
+          userEntered={userEntered}
+          setUserEntered={setUserEntered}
+        />
       ) : (
         <LandingPage setUserEntered={setUserEntered} />
       )}
