f7bfb78e03be753eff5671a2199e8bee314e3a96
Mohit kumar <46507817+Mohitkumar33@users.noreply.github.com>
2022-04-18 11:13:29 +0530

    chore: scroll bar styling done

--- src/components/small components/todoAdded.js
+++ src/components/small components/todoAdded.js
@@ -40,42 +40,44 @@ const TodoAdded = ({ setShowTodo }) => {
           />
         </svg>
       </div>
-
-      {todosList.map((item) => {
-        return (
-          <div className="single-todo" key={item.id}>
-            <label>
-              <input
-                type="checkbox"
-                className="single-todo-checkbox"
-                defaultChecked={item.isDone}
-                onChange={() => handleTaskStatus(item.id)}
-              />
-              <span
-                style={{
-                  textDecoration: item.isDone ? "line-through" : "none",
-                }}
+      <div className="all-todo-notes">
+        {todosList.map((item) => {
+          return (
+            <div className="single-todo" key={item.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  className="single-todo-checkbox"
+                  defaultChecked={item.isDone}
+                  onChange={() => handleTaskStatus(item.id)}
+                />
+                <span
+                  className="todo-note"
+                  style={{
+                    textDecoration: item.isDone ? "line-through" : "none",
+                  }}
+                >
+                  {" "}
+                  {item.todoNote}
+                </span>
+              </label>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="cross-icon"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                onClick={() => deleteTask(item.id)}
               >
-                {" "}
-                {item.todoNote}
-              </span>
-            </label>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="cross-icon"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              onClick={() => deleteTask(item.id)}
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-        );
-      })}
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+          );
+        })}
+      </div>
 
       <input
         type="text"
