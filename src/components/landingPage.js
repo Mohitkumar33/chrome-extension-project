f84a5f90b6973f15d1bc0180ce1bf8650d32d70e
Mohitkumar33 <mohitkumar06612@gmail.com>
2023-04-30 19:05:18 +0530

    testing

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -11,7 +11,7 @@ const LandingPage = ({ setUserEntered }) => {
           "https://res.cloudinary.com/dbfzfqfhl/image/upload/v1649858996/ecom%20item%20images/fredrik-solli-wandem-7wUGrYL6B9c-unsplash_kqvmac.jpg",
       }}
     >
-      <h1 className="welcome-msg">Hello, what's your name?</h1>
+      <h1 className="welcome-msg">Hello, what's your name</h1>
       <input
         className="welcome-input"
         type="text"
